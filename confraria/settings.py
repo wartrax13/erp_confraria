1d86f6e65ff3c7841bdd6625065f7c8eefd68511
pedro <pedro.moisesandrade@gmail.com>
2021-11-20 17:12:30 -0300

    Autenticaçao

--- confraria/settings.py
+++ confraria/settings.py
@@ -28,6 +28,9 @@ DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
--- confraria/urls.py
+++ confraria/urls.py
@@ -17,8 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
 from confraria.pessoa.views import home
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('passwordreset/', auth_views.LogoutView.as_view(), name='password_reset'),
     path('admin/', admin.site.urls),
     path('', home),
     path('pessoa/', include('confraria.pessoa.urls'))
