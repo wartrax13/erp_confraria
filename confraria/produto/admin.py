ca88b97e9e1081d105193535a246b6a358ea6416
realtrends <pedro.moisesandrade@gmail.com>
2021-12-21 14:04:00 -0300

    Movimentacao Produto em Admin

--- confraria/produto/admin.py
+++ confraria/produto/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Produto, Categoria, Movimentacao
-# Register your models here.
+from .models import Produto, Categoria, Movimentacao, MovimentacaoProduto
+
+
 admin.site.register(Produto)
 admin.site.register(Categoria)
 admin.site.register(Movimentacao)
+admin.site.register(MovimentacaoProduto)
